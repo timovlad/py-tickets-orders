d69a8f50eb7ddf5534e6982e7487ef81e8a52c99
Vlad Tymoshko <slradbez@gmail.com>
2025-02-14 14:25:59 +0100

    filter test and pagination

--- cinema/models.py
+++ cinema/models.py
@@ -60,10 +60,6 @@ class MovieSession(models.Model):
     def __str__(self):
         return self.movie.title + " " + str(self.show_time)
 
-    @property
-    def taken_places(self):
-        return self.tickets.values("row", "seat")
-
 
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -89,8 +85,22 @@ class Ticket(models.Model):
     seat = models.IntegerField()
 
     def clean(self):
-        Ticket.validate_seat(
-            self.row, self.movie_session.cinema_hall.seats_in_row, ValueError)
+        for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
+            (self.row, "row", "rows"),
+            (self.seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(
+                self.movie_session.cinema_hall, cinema_hall_attr_name
+            )
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise ValidationError(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                        f"number must be in available range: "
+                        f"(1, {cinema_hall_attr_name}): "
+                        f"(1, {count_attrs})"
+                    }
+                )
 
     def save(
         self,
@@ -111,12 +121,3 @@ class Ticket(models.Model):
 
     class Meta:
         unique_together = ("movie_session", "row", "seat")
-
-    @staticmethod
-    def validate_seat(rows, cinema_hall, error_to_raise):
-        if not (1 <= rows <= cinema_hall.seats_in_row):
-            raise error_to_raise({
-                "rows":
-                    f"rows must be in available range"
-                    f"[1, {cinema_hall.rows}, not {rows}] ",
-            })
